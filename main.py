4d7cec86e4b424001cff3582bee4d325b576abed
Artpukh <artemytemik@gmail.com>
2022-02-08 16:30:50 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import requests
+
+import os
+import sys
+
+import pygame
+import requests
+
+map_request = "http://static-maps.yandex.ru/1.x/?ll=-14.96952040,40.1680&spn=60,60&l=map"
+response = requests.get(map_request)
+
+if not response:
+    print("Ошибка выполнения запроса:")
+    print(map_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(1)
+
+# Запишем полученное изображение в файл.
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
+
+# Инициализируем pygame
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+# Рисуем картинку, загружаемую из только что созданного файла.
+screen.blit(pygame.image.load(map_file), (0, 0))
+# Переключаем экран и ждем закрытия окна.
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
+
+# Удаляем за собой файл с изображением.
+os.remove(map_file)
